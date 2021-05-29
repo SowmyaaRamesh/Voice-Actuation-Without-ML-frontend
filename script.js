2ceb64cbf53311d2b03b3bb2f594d7d0a1de52b0
Sowmya <sowmyaramesh4101@gmail.com>
2021-05-29 18:19:44 +0530

    added styles and js

---
+++ script.js
@@ -0,0 +1,10 @@
+const playAudio = () => {
+  let audioFile = document.getElementById("audioFile").value;
+
+  let audioSrc = audioFile.substr(12);
+  let AudioController = document.getElementById("audioSource");
+  AudioController.setAttribute("src", audioSrc);
+  var audio = document.getElementById("audio");
+  audio.load();
+  //   audio.play();
+};
